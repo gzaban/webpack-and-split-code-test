3c18b6b08d1bfdfdb3991a842dee92f9ce5354ab
Guy Tzaban <guy@panpwr.com>
2018-08-25 16:28:39 +0300

    Add Webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+
+	output: {
+		path: path.resolve('dist'),
+		filename: 'bundle.js'
+	},
+
+	module: {
+		rules: [{
+			test: /\.js$/,
+			use: 'babel-loader'
+		},
+
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader', 'sass-loader']
+				})
+			}
+		]
+	},
+
+	plugins: [
+		new ExtractTextPlugin('css/styles.css')
+	]
+};
